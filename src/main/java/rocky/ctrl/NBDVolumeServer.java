bbe8bcdaf1d8abc5bd78a1595bb223634203a96c
Ben Kim <beomheyn.kim@gmail.com>
2020-06-08 10:39:11 -0400

    Initial commit

---
+++ src/main/java/rocky/ctrl/NBDVolumeServer.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2018 Sam Pullara
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package rocky.ctrl;
+
+import com.google.common.primitives.UnsignedInteger;
+import com.google.common.primitives.UnsignedLong;
+
+import rocky.ctrl.NBD.*;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static rocky.ctrl.NBD.*;
+
+/**
+ * Created by sam on 11/9/14.
+ */
+public class NBDVolumeServer implements Runnable {
+
+  private final Logger log;
+
+  private final DataInputStream in;
+  private final DataOutputStream out;
+  private final String exportName;
+  private final Storage storage;
+
+  public NBDVolumeServer(String exportName, DataInputStream in, DataOutputStream out) throws IOException {
+    this.exportName = exportName;
+    log = Logger.getLogger("NDB: " + exportName);
+    storage = new FDBStorage(exportName);
+    log.info("Mounting " + exportName + " of size " + storage.size());
+    storage.connect();
+    this.in = in;
+    this.out = out;
+  }
+
+  private void writeReplyHeaderAndFlush(long handle) throws IOException {
+    synchronized (out) {
+      out.write(NBD_REPLY_MAGIC_BYTES);
+      out.write(NBD_OK_BYTES);
+      out.writeLong(handle);
+      out.flush();
+    }
+  }
+
+  @Override
+  public void run() {
+    try {
+      out.writeLong(storage.size());
+      out.writeShort(NBD_FLAG_HAS_FLAGS | NBD_FLAG_SEND_FLUSH);
+      out.write(EMPTY_124);
+      out.flush();
+
+      while (true) {
+        int requestMagic = in.readInt();// MAGIC
+        if (requestMagic != NBD_REQUEST_MAGIC) {
+          throw new IllegalArgumentException("Invalid magic number for request: " + requestMagic);
+        }
+        Command requestType = Command.values()[in.readInt()];
+        long handle = in.readLong();
+        UnsignedLong offset = UnsignedLong.fromLongBits(in.readLong());
+        UnsignedInteger requestLength = UnsignedInteger.fromIntBits(in.readInt());
+        if (requestLength.longValue() > Integer.MAX_VALUE) {
+          // We could ultimately support this but it isn't common by any means
+          throw new IllegalArgumentException("Failed to read, length too long: " + requestLength);
+        }
+        switch (requestType) {
+          case READ: {
+            byte[] buffer = new byte[requestLength.intValue()];
+            log.info("Reading " + buffer.length + " from " + offset);
+            storage.read(buffer, offset.intValue()).thenApply($ -> {
+              synchronized (out) {
+                try {
+                  out.write(NBD_REPLY_MAGIC_BYTES);
+                  out.write(NBD_OK_BYTES);
+                  out.writeLong(handle);
+                  out.write(buffer);
+                  out.flush();
+                } catch (IOException e) {
+                  throw new RuntimeException(e);
+                }
+              }
+              return null;
+            });
+            break;
+          }
+          case WRITE: {
+            byte[] buffer = new byte[requestLength.intValue()];
+            in.readFully(buffer);
+            log.info("Writing " + buffer.length + " to " + offset);
+            storage.write(buffer, offset.intValue()).thenApply($ -> {
+              try {
+                writeReplyHeaderAndFlush(handle);
+              } catch (IOException e) {
+                throw new RuntimeException(e);
+              }
+              return null;
+            });
+            break;
+          }
+          case DISCONNECT:
+            log.info("Disconnecting " + exportName);
+            storage.disconnect();
+            return;
+          case FLUSH:
+            log.info("Flushing");
+            long start = System.currentTimeMillis();
+            storage.flush().thenApply($ -> {
+              try {
+                writeReplyHeaderAndFlush(handle);
+              } catch (IOException e) {
+                throw new RuntimeException(e);
+              }
+              log.info("Flush complete: " + (System.currentTimeMillis() - start) + "ms");
+              return null;
+            });
+            break;
+          case TRIM:
+            log.warning("Trim unimplemented");
+            writeReplyHeaderAndFlush(handle);
+            break;
+          case CACHE:
+            log.warning("Cache unimplemented");
+            break;
+        }
+      }
+    } catch (Exception e) {
+      log.log(Level.SEVERE, "Unmounting volume " + exportName, e);
+    }
+  }
+}
---
+++ src/main/java/rocky/ctrl/RockyController.java
@@ -0,0 +1,77 @@
+package rocky.ctrl;
+
+import static rocky.ctrl.NBD.INIT_PASSWD;
+import static rocky.ctrl.NBD.NBD_FLAG_HAS_FLAGS;
+import static rocky.ctrl.NBD.NBD_OPT_EXPORT_NAME;
+import static rocky.ctrl.NBD.OPTS_MAGIC_BYTES;
+
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.google.common.base.Charsets;
+
+//import nbdfdb.NBDVolumeServer;
+
+public class RockyController {
+
+	private static Logger log = Logger.getLogger("NBD");
+	
+	private static Integer port = 10809;
+
+	
+	public static void main (String args[]) throws IOException {
+		System.out.println("Hello, Rocky");
+		
+		ExecutorService es = Executors.newCachedThreadPool();
+	    log.info("Listening for nbd-client connections");
+	    ServerSocket ss = new ServerSocket(port);
+	    while (true) {
+	      Socket accept = ss.accept();
+	      es.submit(() -> {
+	        try {
+	          InetSocketAddress remoteSocketAddress = (InetSocketAddress) accept.getRemoteSocketAddress();
+	          log.info("Client connected from: " + remoteSocketAddress.getAddress().getHostAddress());
+	          DataInputStream in = new DataInputStream(accept.getInputStream());
+	          DataOutputStream out = new DataOutputStream(new BufferedOutputStream(accept.getOutputStream()));
+
+	          out.write(INIT_PASSWD);
+	          out.write(OPTS_MAGIC_BYTES);
+	          out.writeShort(NBD_FLAG_HAS_FLAGS);
+	          out.flush();
+
+	          // TODO: interpret the client flags.
+	          int clientFlags = in.readInt();
+	          long magic = in.readLong();
+	          int opt = in.readInt();
+	          if (opt != NBD_OPT_EXPORT_NAME) {
+	            throw new RuntimeException("We support only EXPORT options");
+	          }
+	          int length = in.readInt();
+	          byte[] bytes = new byte[length];
+	          in.readFully(bytes);
+	          String exportName = new String(bytes, Charsets.UTF_8);
+	          log.info("Connecting client to " + exportName);
+	          NBDVolumeServer nbdVolumeServer = new NBDVolumeServer(exportName, in, out);
+	          log.info("Volume mounted");
+	          nbdVolumeServer.run();
+	        } catch (Throwable e) {
+	          log.log(Level.SEVERE, "Failed to connect", e);
+	          try {
+	            accept.close();
+	          } catch (IOException e1) {
+	            e1.printStackTrace();
+	          }
+	        }
+	      });
+	    }
+	  }
+}
