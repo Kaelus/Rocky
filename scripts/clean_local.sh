f4dcba2c1929d1fe0e0e6652191b17c14772db8d
bhkimhy <beomheynkim@hanyang.ac.kr>
2024-02-12 06:57:21 +0900

    update clean_local.sh to remove not only data files but also their directories for each node

--- scripts/clean_local.sh
+++ scripts/clean_local.sh
@@ -9,17 +9,23 @@ working_dir=..
 
 sudo echo "clean_local.sh starts"
 
-echo "removing dynamodb data file.."
-rm -f $working_dir/data/replication_broker/shared-local-instance.db
+echo "removing dynamodb data.."
+#rm -f $working_dir/data/replication_broker/shared-local-instance.db
+rm -rf $working_dir/data/replication_broker
 
 num_node=`ls ${working_dir}/conf | wc -l`
 i=0
 while [ $i -lt $num_node ]; do
     volume_name=`grep lcvdName ${working_dir}/conf/${i}/rocky_local.cfg | cut -d '=' -f 2`
-    echo "removing fdb volume data file.."
+    echo "removing fdb volume data.."
     ./fdb_delete_volume.sh $volume_name
-    echo "removing leveldb data files.."
-    rm -rf $working_dir/data/$i/testing-*
+    echo "removing leveldb data.."
+    #rm -rf $working_dir/data/$i/testing-*
+    rm -rf $working_dir/data/$i
+    echo "removing log data.."
+    rm -rf $working_dir/log/$i
+    echo "removing conf data.."
+    rm -rf $working_dir/conf/$i
     i=`expr $i + 1`
 done
 
